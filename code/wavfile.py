a07899a206d54f9ce8f4b5126d892ff539925381
mackenzie-norman <115865992+mackenzie-norman@users.noreply.github.com>
2024-10-08 17:27:54 -0700

    some changes, assignment 1

---
+++ code/wavfile.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+from collections import namedtuple
+import numpy as np
+import sys, soundfile
+WaveInfo = namedtuple(
+    "WaveInfo",
+    "nchannels samptype sampwidth sampbytes framerate nframes",
+)
+
+# Read a tone from a wave file.
+def read_wave(filename):
+    with soundfile.SoundFile(filename) as f:
+        if f.format != 'WAV':
+            raise Exception(f"unknown file format {f.format}")
+        nchannels = f.channels
+        framerate = f.samplerate
+        nframes = f.frames
+        if f.subtype.startswith('PCM_'):
+            samptype = 'fixed'
+            sampwidth = int(f.subtype[4:])
+            if sampwidth <= 8:
+                sampbytes = 1
+                dtype = 'uint8'
+            elif sampwidth <= 16:
+                sampbytes = 2
+                dtype = 'int16'
+            elif sampwidth <= 32:
+                sampbytes = 4
+                dtype = 'int32'
+            else:
+                raise Exception(f"unknown PCM size {nbits}")
+            data = f.read(dtype=dtype, always_2d=True)
+        elif f.subtype == 'FLOAT':
+            samptype = 'float'
+            sampwidth = 32
+            sampbytes = 4
+            data = f.read(dtype='float32', always_2d=True)
+        elif f.subtype == 'DOUBLE':
+            samptype = 'float'
+            sampwidth = 64
+            sampbytes = 8
+            data = f.read(dtype='float64', always_2d=True)
+        else:
+            raise Exception(f"unknown sample type {info.subtype}")
+    info = WaveInfo(
+        nchannels,
+        samptype,
+        sampwidth,
+        sampbytes,
+        framerate,
+        nframes,
+    )
+    return (info, data)
+
+inner = False
+for fname in sys.argv[1:]:
+    if inner:
+        print()
+    else:
+        inner = True
+
+    try:
+        info, wav = read_wave(fname)
+    except Exception as e:
+        print(f"{fname}: {e}")
+        continue
+    print(f"name={fname}")
+    print(f"channels={info.nchannels}")
+    print(f"framerate={info.framerate}")
+    print(f"format={info.samptype}")
+    print(f"samplewidth={info.sampwidth}")
+    print(f"frames={info.nframes}")
+    samples = wav.reshape(-1)
+
+    format_mid = 0
+    if info.samptype == 'fixed':
+        if info.sampbytes == 1:
+            format_mid = 1 << (info.sampwidth - 1)
+            format_min = 0
+            format_max = (1 << info.sampwidth) - 1
+        else:
+            format_min = -(1 << (info.sampwidth - 1))
+            format_max = -format_min + 1
+    elif info.samptype == 'float':
+        format_min = -1.0
+        format_max = 1.0
+    else:
+        continue
+
+    sample_min = np.min(samples)
+    print(f"min={sample_min}")
+    splo = (sample_min - format_mid) / format_min
+    if splo > 0:
+        print(f"min_fraction={splo:.3}")
+
+    sample_max = np.max(samples)
+    print(f"max={sample_max}")
+    sphi = (sample_max - format_mid) / format_max
+    if sphi > 0:
+        print(f"max_fraction={sphi:.3}")
+
+if __name__ == "__main__":
+    read_wave("test.wav")
--- notebook.md
+++ notebook.md
@@ -2,10 +2,23 @@
 ## Establishing a format
 Today I was hoping to spend my time getting the repo up and ideally figuring out a format I can repeat for my engineering journal. A cursory google search did not pull up any templates sadly so I decided on this, a date in H1, a entry title in H2 and a line break at the end of the entry.
 ```
-#Date
+# Date
 ## Title
 text
 ---
 ```
 
 --- 
+
+# 10-08-2024
+## Assignment 1
+Worked on assignment 1 today. My major sticking points were
+- numpy wanting to set the dtype as float32, needed to use .astype
+- Code snippet from scipy had some things I couldn't understand why they were there. mostly this in the sin function
+`2. * np.pi *`
+
+That said everything looks good in audacity. I'm not sure what a 440hz wave should sound like
+![img](audacity_clip_assign1.png)
+
+
+---
